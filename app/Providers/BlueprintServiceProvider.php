9ed6e994e050b3308ed52844ff20f4cc9005a616
masataka96 <61012361+masataka96@users.noreply.github.com>
2022-05-23 22:44:37 +0900

    マイグレーション

---
+++ app/Providers/BlueprintServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlueprintServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //データベース用system_columnsを定義
+        Blueprint::macro('system_columns', function () {
+            $this->timestamp('created_at')->nullabel();
+            $this->unsignedBigInteger('created_by')->nullbel();
+            $this->timestamp('updated_at')->nullabel();
+            $this->unsignedBigInteger('updated_by')->nullabel();
+            $this->timestamp('deleted_at')->nullabel();
+            $this->unsignedBigInteger('deleted_by')->nullabel();
+        });
+    }
+}
---
+++ database/migrations/2022_05_23_195443_household_account_book.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class HouseholdAccountBook extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('name',100);
+            $table->string('email',254);
+            $table->string('password',128);
+            $grade->string('grade',50);
+
+            $table->system_columns(); //共通カラム定義の呼び出し
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
