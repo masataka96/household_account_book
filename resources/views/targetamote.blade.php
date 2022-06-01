66664d564a57c5c02891779e68dad2db06b21ccc
sawadash19 <shu19951019@icloud.com>
2022-06-02 01:03:26 +0900

    目標金額画面編集

---
+++ resources/views/targetamote.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>家計簿</title>
+</head>
+<body>
+<header>
+    <h1>家計簿</h1>
+</header>
+<main>
+    <h2>目標金額</h2>
+    <div class="month">
+        <h3>月の収入</h3>
+        <h3>月の支出</h3>
+    </div>
+</main>
+</body>
+</html>
+
+
+
