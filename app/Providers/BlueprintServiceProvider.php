d9b55adbceb123edfbcec267da7ac2fd3dea1fbe
ksgrit <kubo5680@gmail.com>
2022-06-24 16:36:35 +0900

    ログイン画面編集

--- app/Providers/BlueprintServiceProvider.php
+++ app/Providers/BlueprintServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Database\Schema\Blueprint;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -24,13 +25,13 @@ class BlueprintServiceProvider extends ServiceProvider
     public function boot()
     {
         //データベース用system_columnsを定義
-        Blueprint::macro('system_columns', function () {
-            $this->timestamp('created_at')->nullabel();
-            $this->unsignedBigInteger('created_by')->nullbel();
-            $this->timestamp('updated_at')->nullabel();
-            $this->unsignedBigInteger('updated_by')->nullabel();
-            $this->timestamp('deleted_at')->nullabel();
-            $this->unsignedBigInteger('deleted_by')->nullabel();
-        });
+       // Blueprint::macro('system_columns', function () {
+            //$this->timestamp('created_at')->nullabel();
+            //$this->unsignedBigInteger('created_by')->nullbel();
+            //$this->timestamp('updated_at')->nullabel();
+            //$this->unsignedBigInteger('updated_by')->nullabel();
+            //$this->timestamp('deleted_at')->nullabel();
+            //$this->unsignedBigInteger('deleted_by')->nullabel();
+        //});
     }
 }
