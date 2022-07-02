1a43f37bb6513e2b69e64cd9c1e4b4d413c63cce
sawadash19 <shu19951019@icloud.com>
2022-07-02 15:38:17 +0900

    更新

--- database/migrations/2022_05_23_195443_household_account_book.php
+++ database/migrations/2022_05_23_195443_household_account_book.php
@@ -10,6 +10,14 @@ class HouseholdAccountBook extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->integer('id')->autoIncrement();
+
+
+            $table->string('name',100);
+            $table->string('email',254);
+            $table->string('password',128);
+            $table->string('grade',50);
+
+            $table->system_columns(); //共通カラム定義の呼び出し
             $table->integer('user_id')->nullable();
             $table->integer('spending')->nullable();
             $table->integer('income')->nullable();
@@ -17,6 +25,7 @@ class HouseholdAccountBook extends Migration
             $table->date('date');
             $table->integer('amount')->nullable();
             $table->systemColumns(); // 共通カラム定義
+
         });
     }
 
