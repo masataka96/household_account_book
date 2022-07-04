56da754f48d7b676722c8e5d0b1224c8a5c3bbb2
sawadash19 <shu19951019@icloud.com>
2022-07-04 19:51:53 +0900

    更新

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('id')->autoIncrement()->nullable();
+            $table->integer('id')->autoIncrement();
             $table->string('name',100);
             $table->string('email',254);
             $table->string('password',128);
