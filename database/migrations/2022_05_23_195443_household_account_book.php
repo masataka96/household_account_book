eeedec126934b4c7f80214e66ab4a7e5e3e23645
sawadash19 <shu19951019@icloud.com>
2022-07-04 19:36:22 +0900

    更新

--- database/migrations/2022_05_23_195443_household_account_book.php
+++ database/migrations/2022_05_23_195443_household_account_book.php
@@ -9,7 +9,7 @@ class HouseholdAccountBook extends Migration
     public function up()
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->integer('id');
+            $table->integer('id')->autoIncrement;
 
 
             $table->string('name',100);
