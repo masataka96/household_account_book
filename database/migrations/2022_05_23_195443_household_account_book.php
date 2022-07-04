ead8a8f832d8378b37617c6be733a4f7a6fb9306
sawadash19 <shu19951019@icloud.com>
2022-07-04 16:36:41 +0900

    更新

--- database/migrations/2022_05_23_195443_household_account_book.php
+++ database/migrations/2022_05_23_195443_household_account_book.php
@@ -9,10 +9,10 @@ class HouseholdAccountBook extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->integer('id')->autoIncrement();
-            $table->string('name',100);
-            $table->string('email',254);
-            $table->string('password',128);
+            //$table->integer('id')->autoIncrement();
+            //$table->string('name',100);
+            //$table->string('email',254);
+            //$table->string('password',128);
             //$grade->string('grade',50);
 
             $table->system_columns(); //共通カラム定義の呼び出し
