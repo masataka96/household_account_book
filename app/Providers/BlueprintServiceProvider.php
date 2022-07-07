76e8fef7d70b93f0912dbe818a0f4ff54588f2f5
masataka96 <61012361+masataka96@users.noreply.github.com>
2022-07-07 21:46:44 +0900

    エラー解除

--- app/Providers/BlueprintServiceProvider.php
+++ app/Providers/BlueprintServiceProvider.php
@@ -30,14 +30,7 @@ class BlueprintServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blueprint::macro('systemColumns', function () {
-            $this->timestamp('created_at')->nullable();
-            $this->unsignedBigInteger('created_by')->nullable();
-            $this->timestamp('updated_at')->nullable();
-            $this->unsignedBigInteger('updated_by')->nullable();
-            $this->timestamp('deleted_at')->nullable();
-            $this->unsignedBigInteger('deleted_by')->nullable();
-        });
+        //
     }
 
 }
